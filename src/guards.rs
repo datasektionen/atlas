a48532a016f779ecd6d0698b7da356f86680a5ea
Oskar Engström <oskar.fv57@gmail.com>
2026-04-10 02:53:41 +0200

    init: restart

--- src/guards.rs
+++
@@ -1,7 +0,0 @@
-pub mod context;
-pub mod darkmode;
-pub mod headers;
-pub mod lang;
-pub mod nav;
-pub mod scheme;
-pub mod user;
--- src/main.rs
+++ src/main.rs
@@ -1,67 +1,38 @@
-use auth::oidc::OidcClient;
-use errors::ErrorPageGenerator;
-use log::*;
-use rocket::fs::FileServer;
-use sqlx::PgPool;
+use actix_web::{App, HttpResponse, HttpServer, Responder, get, middleware, web::Html};
+use askama::Template;
+use chrono::Local;
+use tracing_subscriber::EnvFilter;
 
-mod auth;
-mod config;
-mod dto;
-mod errors;
-mod filters;
-mod guards;
-mod logging;
-mod models;
-mod routing;
-mod services;
-mod splash;
-mod web;
+#[derive(askama::Template, Debug, askama_web::WebTemplate)]
+#[template(path = "index.html")]
+struct MainPage {
+    name: String,
+}
 
-rust_i18n::i18n!("./locales");
+#[get("/")]
+async fn index() -> impl Responder {
+    // render index template
+    Html::new(
+        MainPage {
+            name: Local::now().to_string(),
+        }
+        .render()
+        .unwrap(),
+    )
+}
 
-#[rocket::launch]
-async fn rocket() -> _ {
-    let config = config::Config::get();
-
-    logging::init_logger(config.verbosity, &config.log_file).expect("Failed to initialize logging");
-
-    debug!("{config:?}");
-
-    let db = PgPool::connect(&config.db_url)
-        .await
-        .expect("Failed to connect to database");
-
-    debug!("Initialized database connection pool");
-
-    sqlx::migrate!("./migrations")
-        .run(&db)
-        .await
-        .expect("Failed to apply database migrations");
-
-    info!("Database migrations successfully applied");
-
-    info!(
-        "Available i18n locales: {:?}",
-        rust_i18n::available_locales!()
-    );
-
-    let oidc_client = OidcClient::new(config.get_oidc_config())
-        .await
-        .expect("Failed to initialize OIDC");
-
-    debug!("Initialized OIDC client");
-
-    let splashes = splash::Splashes::new("./splash.txt");
-
-    debug!("Initialized splashes");
-
-    rocket::custom(config.get_rocket_config())
-        .manage(db)
-        .manage(config)
-        .manage(oidc_client)
-        .manage(splashes)
-        .attach(ErrorPageGenerator)
-        .mount("/", &web::tree())
-        .mount("/static", FileServer::from("./static"))
-        .register("/", web::catchers())
+#[actix_web::main]
+async fn main() -> std::io::Result<()> {
+    tracing_subscriber::fmt()
+        .with_env_filter(EnvFilter::default())
+        .init();
+    HttpServer::new(|| {
+        App::new()
+            .wrap(middleware::Compress::default())
+            .wrap(middleware::Logger::default())
+            .service(index)
+    })
+    .bind(("0.0.0.0", 8080))?
+    .run()
+    .await
 }
